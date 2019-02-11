1579cd3e941df09d39b62eae28e660751417ba49
ngadiono <ngadiono@djavaui.com>
2019-02-11 19:31:52 +0700

    let's push

---
+++ src/components/RecipeList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import RecipeItem from './RecipeItem';
+
+class RecileList extends Component {
+    render() {
+        console.log('this.props', this.props);
+
+        return (
+            <div>
+                {
+                    this.props.favoriteRecipes.length > 0 ?
+                    <h4 className="link"><Link to="/favorites">Favorites</Link></h4>
+                    :
+                    <div></div>
+                }                
+                {
+                    this.props.recipes.map((recipe, index) => {
+                        return (
+                            <RecipeItem key={index} recipe={recipe} favoriteButton={true} />
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return state;
+}
+
+export default connect(mapStateToProps, null)(RecileList);
